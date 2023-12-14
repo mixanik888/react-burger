ec2402c9acb59b25bce11954cbf97ca764f5f389
Mix <mixanik84@mail.ru>
2023-12-14 19:22:27 +0300

    header

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
         
           <Header/>
         
-      </div>  
+        </div>  
         )
     }
 
--- src/components/AppHeader/Header.js
+++ src/components/AppHeader/Header.js
@@ -3,18 +3,21 @@ import  './Header.module.css';
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
 import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import {Box} from '@ya.praktikum/react-developer-burger-ui-components'
 
 export const Header = () => (
     <header>
-      
+             
+        <ul style={{ color: 'white' }} class="p-4">
+            <li className="text text_type_main-default"><a href="#" className='text-decoration'> <BurgerIcon type="primary" class="p-4"/> Конструктор</a></li>
+            <li className="text text_type_main-default"><a href="#" className='text-decoration'> <ListIcon type="primary" class="p-4"/> Лента заказов</a></li>
+        </ul>
+        
+        <Logo />
         
         <ul style={{ color: 'white' }} className="p-4">
-            <li className="text text_type_main-default"><a href=""> <BurgerIcon type="primary" />Конструктор</a></li>
-            <li className="text text_type_main-default"><a href=""> <ListIcon type="primary" />Лента заказов</a></li>
-        </ul>
-        <Logo />
-
+            <li className="text text_type_main-default"><a href="#" className='text-decoration'></a></li>
+            <li className="text text_type_main-default"><a href="#" className='text-decoration'> <BurgerIcon type="primary" /> Личный кабинет</a></li>
+        </ul>  
     
     </header>   
   
