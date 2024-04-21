4a2ce03b63d61fa71999878b816e25c3a66b9a27
MiX <mixanik84@mail.ru>
2024-04-21 15:23:30 +0300

    Небольшие исправления

--- src/components/BurgerIngredients/BurgerIngredients.tsx
+++ src/components/BurgerIngredients/BurgerIngredients.tsx
@@ -7,13 +7,21 @@ import { TElement } from "../../utils/types";
 
 export default function BurgerIngredients() {
 
-  const [current, setCurrent] = React.useState("BunTab");
+  const [currentTab, setCurrentTab] = React.useState("BunTab");
   const data = useSelector((store) => store.data.data1);
 
   const scrollToBun = useRef<HTMLDivElement>(null);
   const scrollToSauce = useRef<HTMLDivElement>(null);
   const scrollToMain = useRef<HTMLDivElement>(null);
   const scrollNullRef = useRef<HTMLDivElement>(null);
+
+  
+  const onTabClick = (tab:any) => {
+    setCurrentTab(tab);
+    const element = document.getElementById(tab);
+    if (element) element.scrollIntoView({behavior: "smooth"});
+  }
+
 
   const buns = React.useMemo(
     () => data.filter((item:TElement) => item.type === "bun"),
@@ -30,49 +38,49 @@ export default function BurgerIngredients() {
     [data]
   );
 
-   const executeScroll = (e: string) => {
-     if (e === "BunTab") {
-      setCurrent(e);
-       scrollToBun.current!.scrollIntoView();
-    }
+  //  const executeScroll = (e: string) => {
+  //    if (e === "BunTab") {
+  //     setCurrent(e);
+  //      scrollToBun.current!.scrollIntoView();
+  //   }
 
-     if (e === "SauceTab") {
-       setCurrent(e);
-      scrollToSauce.current!.scrollIntoView();
-     }
+  //    if (e === "SauceTab") {
+  //      setCurrent(e);
+  //     scrollToSauce.current!.scrollIntoView();
+  //    }
 
-     if (e === "MainTab") {
-       setCurrent(e);
-      scrollToMain.current!.scrollIntoView();
-     }
-   };
+  //    if (e === "MainTab") {
+  //      setCurrent(e);
+  //     scrollToMain.current!.scrollIntoView();
+  //    }
+  //  };
 
-  const OnScroll = () => {
-    const rectBun     = scrollToBun.current!.getBoundingClientRect();
-    const rectSauce   = scrollToSauce.current!.getBoundingClientRect();
-    const rectMain    = scrollToMain.current!.getBoundingClientRect();
-    const nullRef     = scrollNullRef.current!.getBoundingClientRect();
+  // const OnScroll = () => {
+  //   const rectBun     = scrollToBun.current!.getBoundingClientRect();
+  //   const rectSauce   = scrollToSauce.current!.getBoundingClientRect();
+  //   const rectMain    = scrollToMain.current!.getBoundingClientRect();
+  //   const nullRef     = scrollNullRef.current!.getBoundingClientRect();
 
-    if (
-      nullRef.top < rectBun.top &&
-      nullRef.top < rectSauce.top &&
-      nullRef.top < rectMain.top
-    ) {
-      setCurrent("BunTab");
-    } else if (
-      nullRef.top > rectBun.top &&
-      nullRef.top < rectSauce.top &&
-      nullRef.top < rectMain.top
-    ) {
-      setCurrent("SauceTab");
-    } else if (
-      nullRef.top > rectBun.top &&
-      nullRef.top > rectSauce.top &&
-      nullRef.top < rectMain.top
-    ) {
-      setCurrent("MainTab");
-    }
-  };
+  //   if (
+  //     nullRef.top < rectBun.top &&
+  //     nullRef.top < rectSauce.top &&
+  //     nullRef.top < rectMain.top
+  //   ) {
+  //     setCurrent("BunTab");
+  //   } else if (
+  //     nullRef.top > rectBun.top &&
+  //     nullRef.top < rectSauce.top &&
+  //     nullRef.top < rectMain.top
+  //   ) {
+  //     setCurrent("SauceTab");
+  //   } else if (
+  //     nullRef.top > rectBun.top &&
+  //     nullRef.top > rectSauce.top &&
+  //     nullRef.top < rectMain.top
+  //   ) {
+  //     setCurrent("MainTab");
+  //   }
+  // };
 
   return (
     data && (
@@ -85,30 +93,32 @@ export default function BurgerIngredients() {
         </p>
         <div className={styles.containerTab}>
           <Tab
-            value="BunTab"
-            active={current === "BunTab"}
-            onClick={executeScroll}
+            value="buns"
+            active={currentTab === "buns"}
+            onClick={onTabClick}
           >
             Булки
           </Tab>
           <Tab
-            value="SauceTab"
-            active={current === "SauceTab"}
-            onClick={executeScroll}
+            value="sauces"
+            active={currentTab === "sauces"}
+            onClick={onTabClick}
           >
             Соусы
           </Tab>
           <Tab
-            value="MainTab"
-            active={current === "MainTab"}
-            onClick={executeScroll}
+            value="mains"
+            active={currentTab === "mains"}
+            onClick={onTabClick}
           >
             Начинки
           </Tab>
         </div>
-        <div className={styles.scroll} onScroll={OnScroll}>
+        <div className={styles.scroll}>
           <p
             className={`${styles.title} text text_type_main-medium mt-10`}
+            title="Булки"
+            id="buns"
             ref={scrollToBun}
           >
             Булки
@@ -125,6 +135,7 @@ export default function BurgerIngredients() {
           </div>
           <p
             className={`${styles.title} text text_type_main-medium mt-10`}
+            id="sauces"
             ref={scrollToSauce}
           >
             Соусы
@@ -142,6 +153,7 @@ export default function BurgerIngredients() {
           <p
             className={`${styles.title} text text_type_main-medium mt-10`}
             ref={scrollToMain}
+            id="mains"
           >
             Начинки
           </p>
